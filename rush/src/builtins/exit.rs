dfb29cb409a16b8631ea4317acb1c18929c52dc1
lulkien <kien.luuhoang.arch@gmail.com>
2026-01-30 23:34:19 +0700

    chore: refactor some shell builtin modules

--- rush/src/builtins/exit.rs
+++
@@ -1,7 +0,0 @@
-use std::process;
-use abi_stable::std_types::{RString, RVec};
-use rush_interface::ExecResult;
-
-pub extern "C" fn exit(_: RVec<RString>) -> ExecResult {
-    process::exit(0);
-}
--- rush/src/builtins/mod.rs
+++ rush/src/builtins/mod.rs
@@ -1,6 +1,18 @@
-mod prompt;
-mod exit;
+use abi_stable::std_types::{RString, RVec};
+use rush_interface::ExecResult;
+use std::io::{self, Write};
+use std::process;
 
-pub use prompt::prompt;
-pub use exit::exit;
+pub extern "C" fn prompt(_: RVec<RString>) -> ExecResult {
+    print!("$ ");
+    match io::stdout().flush() {
+        Ok(_) => ExecResult::default(),
+        Err(e) => {
+            unreachable!("{e}")
+        }
+    }
+}
 
+pub extern "C" fn exit(_: RVec<RString>) -> ExecResult {
+    process::exit(0);
+}
